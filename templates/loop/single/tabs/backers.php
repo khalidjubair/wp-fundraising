c60e121192eeb0d892e0575cf85e5bc12ae52399
Khalid Jubair <khalidjubair09@gmail.com>
2018-10-18 16:41:05 +0600

    baker tab updated

--- templates/loop/single/tabs/backers.php
+++ templates/loop/single/tabs/backers.php
@@ -1,34 +1,31 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
-global $post, $wpdb;
+global $post;
 $html       = '';
-$prefix     = $wpdb->prefix;
 $product_id = $post->ID;
-$data_array = WP_Fundraising_Actions::wf_get_orders_ID_list_per_campaign();
+$baker_list = WP_Fundraising_Actions::wf_get_orders_ID_list_per_campaign();
+
 $wf_roi = get_post_meta($product_id, '_wf_roi', true);
+$user = wp_get_current_user();
+$author = get_the_author();
+$allowed_roles = array('subscriber', 'administrator');
+if( array_intersect($allowed_roles, $user->roles ) ) {
+    if($user->user_login == $author || $user->roles == 'administrator'){
+        $roi_show = true;
+    }else{
+        $roi_show = false;
+    }
+}
 
-$args = array(
-    'post_type'     => 'shop_order',
-    'post_status'   => array('wc-completed','wc-on-hold'),
-    'post__in'      => $data_array
-);
-$the_query = new WP_Query( $args );
-
-if ( $the_query->have_posts() ) :
-    
-    ?>
-    <div class="xs-backers-lsit-wraprer">
-    <ul class="fundpress-backer-lsit">
-
-    <?php
-    while ( $the_query->have_posts() ) : $the_query->the_post();
-
-        $post_id = get_the_ID();
-        $order = new WC_Order($post_id);
+if(!empty($baker_list)):
+    ?><div class="xs-backers-lsit-wraprer">
+    <ul class="fundpress-backer-lsit"> <?php
+    foreach($baker_list as $order){
+        $price_roi = 0;
+        $order = new WC_Order($order);
         $price = $order->get_total();
         $price_roi = $price + ($price * $wf_roi)/100;
 
-        $date=date_format($order->get_date_completed(),"d, F Y");
         $html .= '<li class="row xs-margin-0">';
         $html .= '<div class="row xs-margin-0 col-md-6 text-left">';
 
@@ -39,7 +36,7 @@ if ( $the_query->have_posts() ) :
         $html .= '<div class="fundpress-backer-info">';
 
         $contributor_show   = get_post_meta( $product_id, '_wf_mark_contributors_as_anonymous', true );
-        $html .= '<h4 class="color-green regular xs-mb-0">'.$date.'</h4>';
+        $html .= '<h4 class="color-green regular xs-mb-0">'.date('F d, Y', strtotime($order->get_date_created())).'</h4>';
         if($contributor_show == 'yes'):
             $html .= '<h3 class="color-navy-blue semi-bold">'.esc_html__('Anonymous','wp-fundraising').'</h3>';
         else:
@@ -53,23 +50,16 @@ if ( $the_query->have_posts() ) :
 
         $html .= '</div>';
         $html .= '<div class="fundpress-backer-info pull-right">';
-        if($order->get_total() != null){
+
+        if($roi_show && $order->get_total() != null){
             $html .= '<h4 class="color-green regular xs-mb-0">Return: '.wc_price($price_roi).'</h4>';
         }
         $html .= '</div>';
         $html .= '</li>';
-
-    endwhile;
-    wp_reset_postdata();
+    }
 
     $html .= '</ul></div>';
-    ?>
-
-
-
-<?php
-else :
+else:
     $html .= esc_html__( 'Sorry, no backer for this campaign.','wp-fundraising' );
 endif;
-
 echo $html;
